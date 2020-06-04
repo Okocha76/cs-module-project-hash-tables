1c7c9cd8c03dad6b2376b293e72fc7406b116e52
Okocha76 <Okocha76@users.noreply.github.com>
2020-06-04 16:42:44 +0300

    Lookup Table, Expensive Sequence, Word Count, No Duplicates

--- applications/word_count/word_count.py
+++ applications/word_count/word_count.py
@@ -1,6 +1,18 @@
 def word_count(s):
-    # Your code here
-
+    word_count = {}
+    ignore_chars = '" : ; , . - + = / \ | [ ] { } ( ) * ^ &'
+    words = s.split()
+    for word in words:
+        for char in ignore_chars:
+            if char in word:
+                word = word.replace(char,'')
+        word = word.lower()
+        if word:
+            if not word in word_count:
+                word_count[word] = 1
+            else:
+                word_count[word] += 1
+    return word_count
 
 
 if __name__ == "__main__":
